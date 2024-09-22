10b3fd721a194b20bc4db658e732fc916cc6d3f3
Daniel Stellmon <Dstellm@gmail.com>
2024-09-22 04:26:07 -0500

    report, draw table, and readSQL work now

--- dataFrame.py
+++ dataFrame.py
@@ -1,18 +1,28 @@
 import pandas as pd
 import sqlite3 as sqlite3
 
-#do data frame stuff
 class dataFrame(object):
-    con = sqlite3.connect("goose.db")
+    conn = sqlite3.connect("goose.db")
 
-#select * from <table> into a data frame
-#print said data frame
-#should not be pulling its own data, that's what the DB interface is for
-    def drawTable(table):
-        con = sqlite3.connect("goose.db") #why does the above con not work?
-        df = pd.read_sql_query('select * from ' + table, con)
+    @classmethod
+    def report(self):
+        df = pd.read_sql_query(self.readSQL('report'), self.conn)
         print(df.to_markdown(index=False, tablefmt='grid'))
-        con.close()
+        print('')
+        #self.conn.close()
+
+    @classmethod
+    def drawTable(self, table):
+        df = pd.read_sql_query('select * from ' + table, self.conn)
+        print(df.to_markdown(index=False, tablefmt='grid'))
+        print('')
+        #self.conn.close()
+
+    @classmethod
+    def readSQL(self, file):
+        with open('./SQL/' + file + '.sql', 'r') as file:
+            sql = file.read()
+        return sql
 
 #pick which row to edit by row id
 #display that in a data frame
