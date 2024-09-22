964c5dbeff92e8fb547177330c5c3d27f85b864d
Daniel Stellmon <Dstellm@gmail.com>
2024-09-22 04:27:03 -0500

    removed DB interface code, added dataframe code

--- main.py
+++ main.py
@@ -3,39 +3,29 @@ from rich.console import Console
 from DBInterface import *
 from dataFrame import *
 import pandas as pd
+import os
 
 DBInterface().create()
 
 console = Console()
 
-report = DBInterface().reportShort()
-
-console.print('You have ' + report[0] + ' Domains registered, ' + report[1] + ' of which are active.')
-console.print('You have ' + report[2] + ' Subdomains registered, ' + report[3] + ' of which are active.')
-console.print('')
+dataFrame().report()
 
 selOptions = [
-    'Register a new domain',
-    'Edit existing domain',
-    'Register a new subdomain',
-    'Edit and existing subdomain'
+    'Domains',
+    'Subdomains',
+    'Core settings'
 ]
-
-con = sqlite3.connect("goose.db")
 
 opt = select(selOptions, cursor="🢧", cursor_style="cyan")
 
 #I wanted to use a switch statement, but in Python the match option does not work that way.
 if opt == selOptions[0]:
-    console.print('new domain')
-elif opt == selOptions[1]:
-    # domainDF = dataFrame()
-    # domainDF().drawTable('domain')
     dataFrame.drawTable('domain')
+    command = input("Index to edit: ")
+elif opt == selOptions[1]:
+    dataFrame.drawTable('subdomains')
 elif opt == selOptions[2]:
-    console.print('new subdomain')
-elif opt == selOptions[3]:
-    df = pd.read_sql_query('select * from Subdomains', con)
-    print(df.to_markdown(index=False, tablefmt='grid'))
+    console.print('Core options')
 else:
     console.print('This should not be possible')
