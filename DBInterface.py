4c5b4b35626d8b4eb255c30217c80af24d9595b4
Daniel Stellmon <Dstellm@gmail.com>
2024-09-22 04:26:23 -0500

    removed code replaced by dataframes

--- DBInterface.py
+++ DBInterface.py
@@ -13,26 +13,8 @@ class DBInterface(object):
             for query in sqlArray:
                 self.cursor.execute(query)
 
-    def maxDomainID(self):
-        self.cursor.execute(self.readSQL('maxDomainID'))
-        return int(self.intConvertTuple(self.cursor.fetchone()))
-
-    def reportShort(self):
-        ReportShortArray = []
-        sqlArray = self.readSQL('reportShort')
-        for query in sqlArray:
-            self.cursor.execute(query)
-            ReportShortArray.append(self.intConvertTuple(self.cursor.fetchone()))
-        return ReportShortArray
-
     def readSQL(self, file):
         with open('./SQL/' + file + '.sql', 'r') as file:
             sql = file.read()
         sqlArray = sql.split(2*os.linesep)
         return sqlArray
-
-    def intConvertTuple(self, tup):
-        return ''.join(map(str, tup))
-
-    def strConvertTuple(self, tup):
-        return ''.join(tup)
